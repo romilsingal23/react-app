ee6c6a9baa119cca087ef4c9141a125562ef6190
rjshk013 <rjshk013@gmail.com>
2024-10-28 20:48:50 +0530

    updated reactjs

--- k8s/reactjs-frontend/entrypoint.sh
+++ k8s/reactjs-frontend/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+# Debugging log to check if REACT_APP_BACKEND_URL is set
+echo "REACT_APP_BACKEND_URL is set to: $REACT_APP_BACKEND_URL"
 # Replace placeholders with actual environment variables
 envsubst < /usr/share/nginx/html/env.template.js > /usr/share/nginx/html/env.js
-
+#cp /usr/share/nginx/html/env.template.js /usr/share/nginx/html/env.js
+echo "env.js content after substitution:"
+cat /usr/share/nginx/html/env.js
 # Start NGINX
 nginx -g 'daemon off;'
--- k8s/reactjs-frontend/src/App.js
+++ k8s/reactjs-frontend/src/App.js
@@ -1,45 +1,31 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom';
+import './App.css';  // Import the custom CSS file
 
-// Use environment variable for backend URL
-const backendURL = window.REACT_APP_BACKEND_URL || 'http://localhost:5000';  // Use a default value if the env variable isn't set
-// Login component
+const backendURL = window.REACT_APP_BACKEND_URL || 'http://localhost:5000';
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
-  const history = useHistory();  // React Router's hook for navigation
+  const history = useHistory();
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    console.log('Attempting login...');
     try {
-      const response = await axios.post(`${backendURL}/login`, { 
-        username, 
-        password 
-      }, {
-        headers: {
-          'Content-Type': 'application/json'
-        }
+      const response = await axios.post(`${backendURL}/login`, { username, password }, {
+        headers: { 'Content-Type': 'application/json' }
       });
-  
-      console.log('Login response:', response.data);
-  
       if (response.data.message === 'Login successful!') {
-        console.log('Login successful, redirecting to /welcome...');
         setMessage('');
         history.push('/welcome');
       } else {
-        console.log('Login failed, displaying invalid credentials message...');
         setMessage('Invalid credentials');
       }
     } catch (error) {
-      console.log('Error during login:', error);
       setMessage('Invalid credentials');
     }
   };
-  
 
   return (
     <div className="App">
@@ -64,17 +50,15 @@ function Login() {
   );
 }
 
-// Welcome component
 function Welcome() {
   return (
-    <div>
+    <div className="dashboard">
       <h1>Welcome to the Dashboard!</h1>
       <p>This is a simple dashboard after login.</p>
     </div>
   );
 }
 
-// Main App component
 function App() {
   return (
     <Router>
@@ -87,4 +71,3 @@ function App() {
 }
 
 export default App;
-
